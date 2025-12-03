e5909384147733693b4808390f3106b5b9b345d2
Eluukin <helinakindu@gmail.com>
2025-12-03 13:27:22 -0800

    Create pg.sql

---
+++ pg.sql
@@ -0,0 +1,12 @@
+multipage-website/
+│
+├── index.html
+├── about.html
+├── contact.html
+├── css/
+│   └── styles.css
+├── js/
+│   └── script.js
+├── images/
+│   └── (any images you use)
+└── README.md
